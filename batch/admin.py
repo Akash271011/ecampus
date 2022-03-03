873c1bc614124bdd97e08277a7564954b046b3c5
Akash <rathodakash788@gmail.com>
2022-03-03 19:54:39 +0530

    table creation and templates integration

---
+++ batch/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Batch, BatchDetail, BatchTime
+
+# Register your models here.
+admin.site.register(Batch)
+admin.site.register(BatchDetail)
+admin.site.register(BatchTime)
